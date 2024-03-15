2410cfc1c80d2a42ed0d18923de7c21a2461907c
Milo Mordaunt <milomord@gmail.com>
2024-03-15 14:27:58 -0400

    Add a cheeky basepath

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'wouter'
+import { Router, Route, Switch } from 'wouter'
 import { SWRConfig } from 'swr'
 import api from  './api'
 import GameApp from './GameApp'
@@ -16,16 +16,18 @@ const swrConfig = {
 function App() {
   return (
     <SWRConfig value={swrConfig}>
-      <Switch>
-        <Route path="/">
-          <GameApp />
-        </Route>
-        <Route path="/levels/:id">
-          {params => <GameApp levelId={params.id} />}
-        </Route>
+      <Router base={import.meta.env.PROD ? '/dust-ts' : ''}>
+        <Switch>
+          <Route path="/">
+            <GameApp />
+          </Route>
+          <Route path="/levels/:id">
+            {params => <GameApp levelId={params.id} />}
+          </Route>
 
-        <Route>404: No such page!</Route>
-      </Switch>
+          <Route>404: No such page!</Route>
+        </Switch>
+      </Router>
     </SWRConfig>
   )
 }
