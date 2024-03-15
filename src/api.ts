ca4a73aac567931ae5307244eecfe23a3cf09c73
Milo Mordaunt <milomord@gmail.com>
2024-03-15 14:21:54 -0400

    Attempt to use fly API

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import wretch from 'wretch'
 import QueryStringAddon from "wretch/addons/queryString"
 
-const API_URL = import.meta.env.DUST_API_URL || 'http://localhost:8080'
+const API_URL = import.meta.env.PROD ? 'https://dust-server.fly.dev' : 'http://localhost:8080'
 const api = wretch(API_URL).addon(QueryStringAddon)
 
 export default api
